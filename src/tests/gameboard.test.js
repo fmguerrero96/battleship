6c65c78ee5044a6d4f54dfadca0c508fbb23e3c1
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-20 23:19:03 -0400

    Write test for receiveAttack function to check when an attack hits a ship

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -29,9 +29,22 @@ describe('gameboardFactory', () => {
         })
         test('if attack misses, grid will be 0 at coordinates', () => {
             const board = gameboardFactory()
-            const coordinates = [4,3]
+            const coordinates = [2,3]
             board.receiveAttack(coordinates)
-            expect(board.grid[4][3]).toEqual(0)
+            expect(board.grid[2][3]).toEqual(0)
+        })
+        test('if attack hits, the ship will register the hit', () => {
+            const board = gameboardFactory()
+            const length = 3
+            const index = [2, 3]
+            board.placeShip(index, length)
+            board.receiveAttack(index)
+            expect(board.ships[0].hit()).toBe(2)
+            /*The ship was only hit once, but I called the hit function again
+            so that it would return a value. So the ship was hit once inside 
+            the receive attack function and once more in the 'expect' line.
+            Ultimately the function works as expected and the test*/
+             
         })
     })
 })
