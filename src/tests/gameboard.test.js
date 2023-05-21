83422b7bf497d01a94893e9ca9d1ea0e43e197e7
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-20 21:37:20 -0400

    Add test for gameboard to verify that error is thrown

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,11 +6,16 @@ describe('gameboardFactory', () => {
         const length = 3
         const index = [2, 3]
         board.placeShip(index, length)
-        for (let i = 0; i < length; i++) {
-            expect(board.grid[2][3 + i]).toBeInstanceOf(Object);
-        }
+        expect(board.grid[2][3]).toBeInstanceOf(Object);
+        expect(board.grid[2][4]).toBeInstanceOf(Object); 
+        expect(board.grid[2][5]).toBeInstanceOf(Object);
         //Check that the ship is added to the ships array
         expect(board.ships.length).toBe(1); 
+    })
+    test('cannot place ship on occupied spots', () => {
+        const length = 2
+        const index = [2, 3]
+        expect(() => board.placeShip(index, length)).toThrowError('Cannot place ship on an occupied spot')
     })
 })
 
