83dc57d64372f2001e817a8bf3266ee08866d90e
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-28 20:19:23 -0400

    create function to add a ship on the board by clicking on a cell and create event listener to close the modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import { events } from "./modules/dom";
+import events from "./modules/dom";
+import playerFactory from "./modules/player"
+import gameboardFactory from "./modules/gameboard"
 
-events.createCells('.pre-board')
-events.createCells('.human')
-events.createCells('.computer')
+const humanPlayer = playerFactory('human')
+const aiPlayer = playerFactory('computer')
+
+const humanGameboard = gameboardFactory()
+const aiGameboard = gameboardFactory()
+
+const domEvents = events(humanGameboard)
+
+domEvents.createCells('.pre-board')
+domEvents.createCells('.human')
+domEvents.createCells('.computer')
+
+domEvents.insertShip()
+domEvents.closeModal()
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,4 +1,7 @@
-const events = (() => {
+const events = ((board) => {
+    const shipLengths = [5, 4, 3, 3, 2];
+    let thisShip = 0
+
     const createCells = (container) => {
         const dimensions = 10
         const board = document.querySelector(container)
@@ -7,16 +10,39 @@ const events = (() => {
     
         let numDivs = dimensions * dimensions;
         for (let i = 0; i < numDivs; i++){
-            let cell = document.createElement('div'); //creating cells
+            let cell = document.createElement('div'); 
             cell.className = 'cell'
             const x = i % dimensions; // Calculate x coordinate
             const y = Math.floor(i / dimensions); // Calculate y coordinate
             cell.setAttribute('data-coordinate', `[${x}, ${y}]`)
-            board.insertAdjacentElement("beforeend", cell)//inserting cells into container
+            board.insertAdjacentElement("beforeend", cell)
         }
     }
 
-    return {createCells}
-})();
+    const retrieveCoordinates = (event) => {
+        const clickedCell = event.target
+        const coordinates = clickedCell.getAttribute('data-coordinate')
+        const [x, y] = JSON.parse(coordinates)
+        
+        board.placeShip([x, y],shipLengths[thisShip])
+        thisShip++
+        console.log(thisShip)
+    }
 
-export {events}
+    const insertShip = () => {
+        const preBoard = document.querySelector('.pre-board')
+        preBoard.addEventListener('click', (retrieveCoordinates))
+    }
+
+    const closeModal = () => {
+        const begin = document.querySelector('.begin')
+        const welcomeScreen = document.querySelector('.welcomeScreen')
+        begin.addEventListener('click', () => {
+            welcomeScreen.close()
+        })
+    }
+
+    return {createCells, insertShip, closeModal}
+});
+
+export default events
