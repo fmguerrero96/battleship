f02120cca464f43b7a548e90775f3bc04979f5f0
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-29 23:55:08 -0400

    call placeRandomShips on aiGameboard to populate it with ships

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import events from "./modules/dom";
 import playerFactory from "./modules/player"
 import gameboardFactory from "./modules/gameboard"
 
-const humanPlayer = playerFactory('human')
+const humanPlayer = playerFactory('human') //create player instances
 const aiPlayer = playerFactory('computer')
 
-const humanGameboard = gameboardFactory()
+const humanGameboard = gameboardFactory() //create board instances 
 const aiGameboard = gameboardFactory()
+
+aiGameboard.placeRandomShips() //populate ai board with ships
 
 const domEvents = events(humanGameboard)
 
